ebc987591d1d115a0564b7ed7078f7701c334ddc
olyesung <olyesung@gmail.com>
2022-03-21 17:14:17 +0900

    1.1 TailwindCSS Setup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { reactRoot: true },
+  experimental: { reactRoot: "concurrent" },
 };
 
 module.exports = nextConfig;
